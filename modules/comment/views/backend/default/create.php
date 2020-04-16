07f04f626874549f4fcc68cb69d5111acada0375
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-04-16 20:49:36 +0300

    commit

--- modules/comment/views/backend/default/create.php
+++ modules/comment/views/backend/default/create.php
@@ -1,20 +1,35 @@
 <?php
 
 use yii\helpers\Html;
+use modules\comment\Module;
 
 /* @var $this yii\web\View */
 /* @var $model modules\comment\models\Comment */
 
-$this->title = 'Create Comment';
-$this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = Module::t('module', 'Comments');
+$this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['index']];
+$this->params['breadcrumbs'][] = Module::t('module', 'New Comment');
 ?>
-<div class="comment-create">
+<div class="comment-backend-default-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('form/_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><?= Html::encode(Module::t('module', 'New Comment')) ?></h3>
+            <div class="box-tools pull-right"></div>
+        </div>
+        <div class="box-body">
+            <div class="pull-left"></div>
+            <div class="pull-right"></div>
+            <?= $this->render('form/_create', [
+                'model' => $model,
+            ]) ?>
+        </div>
+        <div class="box-footer">
+            <?= Html::submitButton('<span class="glyphicon glyphicon-plus"></span> ' . Module::t('module', 'Create'), [
+                'class' => 'btn btn-success',
+                'form' => 'form-create'
+            ]) ?>
+        </div>
+    </div>
 
 </div>
---
+++ modules/comment/views/backend/default/form/_create.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+use modules\comment\Module;
+use modules\comment\models\Comment;
+use modules\comment\assets\CommentAsset;
+
+/* @var $this yii\web\View */
+/* @var $model modules\comment\models\Comment */
+/* @var $form yii\widgets\ActiveForm */
+
+$categoryAsset = new CommentAsset([
+    'url' => Url::to(['children-list'])
+]);
+$categoryAsset::register($this);
+?>
+
+<div class="comment-form-create">
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'form-create'
+    ]); ?>
+
+    <?= $form->field($model, 'parentId')->dropDownList(Comment::getCommentTree(), [
+        'id' => 'input-parent-id',
+        'prompt' => Module::t('module', 'No Parent (saved as root)'),
+    ])->label(Module::t('module', 'Parent')) ?>
+
+    <div id="children-list-container" style="display:none;">
+        <?= $form->field($model, 'childrenList')->listBox(Comment::getChildrenList(), [
+            'id' => 'input-children-list',
+        ]) ?>
+        <?= $form->field($model, 'typeMove')->radioList(Comment::getMoveTypesArray(), [
+            'id' => 'input-type-move',
+        ]) ?>
+    </div>
+
+    <?= $form->field($model, 'entity')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'entity_id')->textInput() ?>
+
+    <?= $form->field($model, 'author')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'status')->dropDownList(Comment::getStatusesArray()) ?>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
